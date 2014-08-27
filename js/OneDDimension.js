65642e09eb8615d252c896838945e86ea46cc271
Jan Jezek <jezekjan@kiv.zcu.cz>
2014-08-27 23:00:38 +0200

    Prototype of gilter raster woring!

--- js/OneDDimension.js
+++ js/OneDDimension.js
@@ -56,6 +56,8 @@ function OneDDimension(manager) {
 		gl.bindTexture(gl.TEXTURE_2D, restexture);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE); //Prevents s-coordinate wrapping (repeating).
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, framebuffer.width,
 				framebuffer.height, 0, gl.RGBA, gl.FLOAT, null);
