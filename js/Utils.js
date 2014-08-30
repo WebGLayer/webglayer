1936ffca4bad82b547b1f173144dc75efa1de2bc
Jan Jezek <jezekjan@kiv.zcu.cz>
2014-08-30 15:56:45 +0200

    utils added

---
+++ js/Utils.js
@@ -0,0 +1,56 @@
+Utils = function(){}
+Utils.prototype.loadShaders = function(vs, fs){	
+	var vertexSrc = document.getElementById(vs).text;
+	var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertexShader, vertexSrc);
+	gl.compileShader(vertexShader);
+
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		alert("An error occurred compiling the shaders: "
+				+ gl.getShaderInfoLog(vertexShader));
+		return null;
+	}
+	// create fragment shader
+	var fragmentSrc = document.getElementById(fs).text;
+	var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShader, fragmentSrc);
+	gl.compileShader(fragmentShader);
+
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		alert("An error occurred compiling the shaders: "
+				+ gl.getShaderInfoLog(vertexShader));
+		return null;
+	}
+	// link shaders to create our program
+	var pointProgram = gl.createProgram();
+	gl.attachShader(pointProgram, vertexShader);
+	gl.attachShader(pointProgram, fragmentShader);
+	gl.linkProgram(pointProgram);
+	return pointProgram;
+}
+
+Utils.prototype.bindUniform = function(prog, u_name, value){
+	gl.useProgram(prog);
+	var matrixLoc = gl.getUniformLocation(prog, u_name);
+	if (matrixLoc instanceof WebGLUniformLocation && value!=null){
+		gl.uniformMatrix4fv(matrixLoc, false, value);
+	} else {
+		console.error("Uniform set failed, uniform: "+u_name + " value "+value);
+	}
+	gl.useProgram(null);
+}
+
+Utils.prototype.enableAttribute = function(prog, buf){	
+	gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+	var attributeLoc = gl.getAttribLocation(prog, buf.attr);
+	if (attributeLoc >= 0){
+		gl.enableVertexAttribArray(attributeLoc);
+		gl.vertexAttribPointer(attributeLoc, 2, gl.FLOAT, false, 0, 0);
+	} else {
+		console.error("Error binding buffer: "+buf);
+	}
+	
+
+}
+
+
