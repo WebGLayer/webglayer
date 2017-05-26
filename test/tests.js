7e9a3c91eec48d3d02c173049071a9f3d309b957
František Kolovský <f.kolovsky@gmail.com>
2017-05-26 12:13:46 +0200

    added clenAll(true) to the Tests

--- test/tests.js
+++ test/tests.js
@@ -26,13 +26,29 @@ QUnit.test("Delete dimensions", function( assert ) {
     }
     assert.equal(len, 0, "length of the WGL._dimension array after clean POINT");
 
+    WGL.addHeatMapDimension(data.pts, 'heatmap');
+    WGL.addMapDimension(data.pts, 'themap');
+
+    var set = {data: data.value,  min:0, max:100, num_bins: 3, name: 'value',type:'linear', label :"value"};
+    WGL.addLinearHistDimension(set);
+    WGL.addLinearFilter(set, 3, 'valueF');
+
+    WGL.addExtentFilter();
+
+    WGL.cleanAll(true);
+    var len = 0;
+    for (var key in WGL._dimensions){
+        len += 1;
+    }
+    assert.equal(len, 0, "length of the WGL._dimension array after cleanAll(true)");
+
     var det_TA = WGL.utils.array2TA(data.value);
     WGL.getManager().addDataBuffer(det_TA, 1, 'dimension', 10, 30);
 
     var heatLines = WGL.addMapLineDimension(data.pts, 'themap2');
     heatLines.setValuesBuffer('dimension');
 
-    WGL.cleanAll();
+    WGL.cleanAll(true);
 
     var len = 0;
     for (var key in WGL._dimensions){
