97772a3778227c3555d95bb0362840155ea5ee75
František Kolovský <f.kolovsky@gmail.com>
2018-02-21 10:25:39 +0100

    impruve and fix build files

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,38 +13,45 @@ module.exports = function(grunt) {
     	    dest : 'build/main.js'
     	  }
     	},
-    	
-    	
+
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['env']
+      },
+      dist: {
+        files: {
+          'build/main_es5.js': 'build/main.js'
+        }
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle:true
       },
       build: {
-        src: 'build/main.js',
+        src: 'build/main_es5.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
     	
     jsdoc : {
-    	        dist : {
-    	            src: ['src/*'],
-    	            options: {
-    	                destination: 'doc'
-    	            }
-    	        }
-    	    },
-    qunit: {
-        all: ['test/**/*.html']
+      dist : {
+        src: ['src/*'],
+        options: {
+            destination: 'doc'
+        }
       }
+    }
   });
 
-  // Load the plugin that provides the "uglify" task.''
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-jsdoc');
-  // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify']);
+
+  grunt.registerTask('default', ['concat','babel', 'uglify']);
 
 };
