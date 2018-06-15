7e01ae2811df4f482c8e53522cbf859bd83cb0f4
František Kolovský <f.kolovsky@gmail.com>
2018-06-15 12:58:47 +0200

    delete white lines

--- shaders/shaders_flags.glsl
+++ shaders/shaders_flags.glsl
@@ -1,9 +1,7 @@
 <script id="flags_vShader" type="x-shader/x-vertex">
-
 	attribute vec2 attr;
 	//attribute float value;
 	attribute vec2 index;
-
 
 	uniform mat4 rasterMatrix;
 	uniform sampler2D filter;
@@ -12,33 +10,25 @@
 	varying vec4 col;
 
 	void main() {
-
-
-
 		gl_Position = vec4((attr[0]*2.)-1., (attr[1]*2.)-1., 0., 1.);
   		gl_PointSize = 1.;
 
   		//vec4 rp = rasterMatrix * vec4(index[0],index[1],0.,1.);
   		vec4 fdata = texture2D(filter, vec2((index[0] +1.)/2. , (index[1]+1.)/2.));
   		float val = 1.;
+        //if (fdata[0]>= ( (pow(2.,numfilters-1.)) / 256.) && numfilters != 0. ){
+        if (fdata[3]>0.){
+            // data are out of the window
+            col = vec4(0., 0. , val, 1.);
 
-  			//if (fdata[0]>= ( (pow(2.,numfilters-1.)) / 256.) && numfilters != 0. ){
-  			if (fdata[3]>0.){
-				// data are out of the window
-  				col = vec4(0., 0. , val, 1.);
-
-  			}
-  			else if (fdata[0]>=  ( (numfilters ) / 256.) && numfilters != 0. ){
-  				// data are selected
-				col = vec4(val, 0. , 0., 1.);
-			} else {
-  				// data visible but not selected
-  				col = vec4(0., val , 0., 1.);
-  			}
-
-
-
-
+        }
+        else if (fdata[0]>=  ( (numfilters ) / 256.) && numfilters != 0. ){
+            // data are selected
+            col = vec4(val, 0. , 0., 1.);
+        } else {
+            // data visible but not selected
+            col = vec4(0., val , 0., 1.);
+        }
 	}
 </script>
 
